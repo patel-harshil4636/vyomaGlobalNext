c665326424117df070db9e38fa77cad66fda7795
patel-harshil4636 <patelharshil4636@gmail.com>
2025-11-28 16:10:50 +0530

    Updated page.js

--- app/page.js
+++ app/page.js
@@ -17,37 +17,37 @@ export default function Home() {
 
 const BASE_URL = 'https://vyomaglobal.info';
 
-// export const metadata = {
-//   robots: {
-//     index: true, 
-//     follow: true,
-//   },
-//   metadataBase: new URL(BASE_URL),
-//   title: "VyomaGlobal | Website & Branding Experts",
-//   description: "Grow your business online with VyomaGlobal — from design to deployment.",
-//   icons: {
-//     icon: 'https://vyomaglobal.info/"/vyoma-global.png',
-//     // The sizes property is where you specify the dimensions, matching your HTML
-//     sizes: '16x16', 
-//     type: 'image/png', // Use the correct MIME type for PNG
-//   },
-//   openGraph: {
-//     title: "VyomaGlobal — Build Your Digital Future",
-//     description: "Transform your business with VyomaGlobal.",
-//     url: "https://vyomaglobal.info/",
-//     images: [
-//       {
-//         url: "/vyoma-global.png",
-//         width: 800,
-//         height: 600,
-//         alt: "VyomaGlobal Website Preview",
-//       },
-//     ],
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: "VyomaGlobal | Web Experts",
-//     description: "Build websites that rank & perform.",
-//     images: ["/vyoma-global.png"],
-//   },
-// };
+export const metadata = {
+  robots: {
+    index: true, 
+    follow: true,
+  },
+  metadataBase: new URL(BASE_URL),
+  title: "VyomaGlobal | Website & Branding Experts",
+  description: "Grow your business online with VyomaGlobal — from design to deployment.",
+  icons: {
+    icon: 'https://vyomaglobal.info/"/vyoma-global.png',
+    // The sizes property is where you specify the dimensions, matching your HTML
+    sizes: '16x16', 
+    type: 'image/png', // Use the correct MIME type for PNG
+  },
+  openGraph: {
+    title: "VyomaGlobal — Build Your Digital Future",
+    description: "Transform your business with VyomaGlobal.",
+    url: "https://vyomaglobal.info/",
+    images: [
+      {
+        url: "/vyoma-global.png",
+        width: 800,
+        height: 600,
+        alt: "VyomaGlobal Website Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "VyomaGlobal | Web Experts",
+    description: "Build websites that rank & perform.",
+    images: ["/vyoma-global.png"],
+  },
+};
